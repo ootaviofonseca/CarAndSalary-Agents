996c798379e0ce32ba517bf43a4fd45030959420
Otavio Fonseca <otavioaf123@gmail.com>
2024-10-09 17:39:12 -0300

    Ajustes

--- main.py
+++ main.py
@@ -6,24 +6,21 @@ load_dotenv()
 
 #pergunta = input(": ")
 
-pergunta = "Qual salario de Keven Norman?"
-pergunta = "How much can Keven Norman spend on one installment?"
-pergunta = "Qual carro mais caro?"
-pergunta = "Baseado no salario de Keven Norman, qual o maximo que ele pode gastar em uma parcela de carro?"
-pergunta = "Indique carros que cabem no bolso de Donte Richard?"
-
-pergunta = "Qual carro mais caro e o valor da parcela para Boris Gibson?"
-
-pergunta = "Boris Gibson precisaria parcelar de quantas vezes a Silverado ano 2020?"
+question = "Qual salario de Keven Norman?"
+question = "How much can Keven Norman spend on one installment?"
+question = "Qual carro mais caro?"
+question = "Baseado no salario de Keven Norman, qual o maximo que ele pode gastar em uma parcela de carro?"
+question = "Indique carros que cabem no bolso de Donte Richard?"
+question = "Qual carro mais caro e o valor da parcela para Boris Gibson?"
+question = "Boris Gibson precisaria parcelar de quantas vezes a Silverado ano 2020?"
 
 agent = AgenteOpenAIFunctions()
 executor = AgentExecutor(
     agent = agent.agent, 
     tools = agent.tools, 
-    verbose=True,
+    verbose=False,
     handle_parsing_errors=True)
 
 
-
-resposta = executor.invoke({"input" : pergunta})
+resposta = executor.invoke({"input" : question})
 print(resposta)
